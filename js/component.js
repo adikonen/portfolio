ac732382ed61cc7e9a1b98cf43030faa70ca4765
adikonen <addykonen80@gmail.com>
2023-04-13 22:49:59 +0800

    'responsive'

--- js/component.js
+++ js/component.js
@@ -55,6 +55,16 @@ class TelephoneIcon extends HTMLElement {
       </svg>`;
     }
 }
+
+class TopArrowIcon extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-up-short" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"/>
+        </svg>
+        `;
+    }
+}
 const elements = [
     {name: 'hamburger-icon', customclass: HamburgerIcon},
     {name: 'github-icon', customclass: GithubIcon},
@@ -62,6 +72,7 @@ const elements = [
     {name: 'instagram-icon', customclass: InstagramIcon},
     {name: 'email-icon', customclass: EmailIcon},
     {name: 'telephone-icon', customclass: TelephoneIcon},
+    {name: 'top-arrow-icon', customclass: TopArrowIcon},
 ];
 
 elements.forEach((element) => {
